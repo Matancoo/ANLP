8fc60d67cb364af58e11f56fd3e390f3deb7e2df
Matancoo <103173876+Matancoo@users.noreply.github.com>
2023-05-23 21:18:16 +0300

    Update ex1.py

--- exercise_1/ex1.py
+++ exercise_1/ex1.py
@@ -8,23 +8,13 @@ import sys
 import argparse
 import wandb
 import time
-TODO:
-    -upload train_loss.png 
-# Command Line Parsing:
-# assume args are legal values
-parser = argparse.ArgumentParser()
-parser.add_argument('--seed_num', type=int, required=True)
-parser.add_argument('--training_count', type=int, required=True)
-parser.add_argument('--validation_count', type=int, required=True)
-parser.add_argument('--prediction_count', type=int, required=True)
-args = parser.parse_args()
+# TODO:
+#     -upload train_loss.png 
+
+RESULTS_FILE_PATH = '/res.txt'
+PREDICTIONS_FILE_PATH = '/predictions.txt'
 TOTAL_TRAINING_TIME = 0
-SEEDS: list = list(range(args.seed_num))
-########################################################################################################################
-#model1
-#mdoel2
-#model3 checkoint/accuracy (inex is seed chosen)
-# run following code for each model
+
 
 ########################################################## HELPERS #####################################################
 def tokenize_function(example):
@@ -85,59 +75,82 @@ def write_results(checkpoints,models_parameters,res_directory):
          f.close()
 ########################################################################################################################
 
-# Checkpoints for models 
-checkpoints = ['bert-base-uncased','roberta-base','google/electra-base-generator']
-best_model_output_dir: str = ''
-models_parameters = [] # tupple of (mean_accuracy,accuracy_std) for each model
+def main(args):
+    raw_dataset = load_dataset('sst2')  # Standford Sentiment Treebank
+    tokenized_dataset = raw_dataset.map(tokenize_function,fn_kwargs={'tokenizer':tokenizer}, batched=True)
+    train_dataset = tokenized_dataset['train'] if training_count == -1 else dataset['train'].select(range(training_count))
+    validation_dataset = tokenized_dataset['validation'] if validation_count == -1 else dataset['validation'].select(range(validation_count))
+    # Checkpoints for models 
+    checkpoints = ['bert-base-uncased','roberta-base','google/electra-base-generator']
+#     best_model_output_dir = ''
+#     best_checkpoint = ''
+#     best_seed_model = ''
+    models_parameters = [] # tupple of (mean_accuracy,accuracy_std) for each model
 
-# Pre-processing
-raw_dataset = load_dataset('sst2')  # Standford Sentiment Treebank
-tokenized_dataset = raw_dataset.map(tokenize_function,fn_kwargs={'tokenizer':tokenizer}, batched=True)
-train_dataset = tokenized_dataset['train'] if training_count == -1 else dataset['train'].select(range(training_count))
-validation_dataset = tokenized_dataset['validation'] if validation_count == -1 else dataset['validation'].select(range(validation_count))
 
-# train each model on each seed and record metrics
-for checkpoint in checkpoints:
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    model_accuracies = []
-    for seed in SEEDS:
-        start_time = time.time()
-        trainer = train_model(seed,checkpoint,output_dir,train_dataset,validation_dataset,data_collator,tokenizer)
-        end_time = time.time()
-        training_time = end_time-start_time
-        TOTAL_TRAINING_TIME += training_time 
-        metrics = trainer.evaluate()
-        model_accuracies.append(metrics['eval_accuracy'])
+    # train each model on each seed and record metrics
+    for checkpoint in checkpoints:
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+        model_accuracies = []
+        for seed in range(args.seed_num)):
+            start_time = time.time()
+            
+            if best_seed_model:
+                
+            trainer = train_model(seed,checkpoint,output_dir,train_dataset,validation_dataset,data_collator,tokenizer)
+            end_time = time.time()
+            training_time = end_time-start_time
+            TOTAL_TRAINING_TIME += training_time 
+            metrics = trainer.evaluate()
+            model_accuracies.append(metrics['eval_accuracy'])
+
+        # calculating mean & std of accuracies of models trained on different seeds
+        mean_accuracy = np.mean(model_accuracies)
+        accuracy_std = np.std(model_accuracies)
+        models_parameters.append(mean_accuracy,accuracy_std)
         
-    # calculating mean & std of accuracies of models trained on different seeds
-    mean_accuracy = np.mean(model_accuracies)
-    accuracy_std = np.std(model_accuracies)
-    models_parameters.append(mean_accuracy,accuracy_std)
-    
-# updating res.txt as specified in Exercise
-write_results(checkpoints,models_parameters'./res.txt')
-
-# Evaluation on testset using best model
-checkpoint = 'bert-base-uncased'#need best checkpoint
-test_dataset = raw_dataset['test]
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-classification_pipeline = pipeline("text-classification", model=best_model_output_dir, tokenizer=tokenizer)
-test_sentences = test_dataset['sentence']
-test_labels = test_dataset['label']
-predictions = classification_pipeline(test_sentences)
-predicted_labels = [prediction['label'] for prediction in predictions]
-
-# writing perdiction.txt as specified in exercise
-with open('path','a') as f:
-  for tup in list(zip(test_sentences,predicted_labels)):
-    line = tup[0] + "###" + tup[1][-1] + "\n"
-    f.write(line)
-  f.close
+        # compaire models to save only the best 
+#         if best_model_output_dir:
+            
 
 
-    
+    # updating res.txt as specified in Exercise
+    write_results(checkpoints,models_parameters'./res.txt')
 
+    # Evaluation on testset using best model
+    test_dataset = raw_dataset['test]
+    tokenizer = AutoTokenizer.from_pretrained(best_checkpoint)
+    classification_pipeline = pipeline("text-classification", model=best_model_output_dir, tokenizer=tokenizer)
+    start_time = time.time() 
+                              
+    test_sentences = test_dataset['sentence']
+    test_labels = test_dataset['label']
+                               
+    predictions = classification_pipeline(test_sentences)
+    end_time = time.time()
+    pred_time = end_time-start_time                           
+    with open('path','a') as f:
+                               
+    predicted_labels = [prediction['label'] for prediction in predictions]
+    # TOSO: get accuuracy
+    # writing perdiction.txt as specified in exercise
+    with open('path','a') as f:
+      for tup in list(zip(test_sentences,predicted_labels)):
+        line = tup[0] + "###" + tup[1][-1] + "\n"
+        f.write(line)
+      f.close
+
+
+    if __name__ == "__main__":
+    # Command Line Parsing: # assume args are legal values
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed_num', type=int, required=True)
+    parser.add_argument('--training_count', type=int, required=True)
+    parser.add_argument('--validation_count', type=int, required=True)
+    parser.add_argument('--prediction_count', type=int, required=True)
+    args = parser.parse_args()
+    main(args)
 
         
     
